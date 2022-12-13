c73f75c157d22034d1a73c2101488342221c7554
Minoo Ashoori <120224294@umail.ucc.ie>
2022-12-13 23:19:34 +0000

    the time spent below 63 for transient and signal has been fized; the possibility to separate first 6 hours has been added

--- transient_feature_extraction.m
+++ transient_feature_extraction.m
@@ -1,8 +1,9 @@
-P = 'C:\Users\PhysioUser\Desktop\PhD\Monitor\Minoo\pre-processed signals\Transients';
+P = 'C:\Users\PhysioUser\OneDrive - University College Cork\Monitor\Minoo\pre-processed signals\Transients';
 S = dir(fullfile(P,'*.txt')); 
-PP = 'C:\Users\PhysioUser\Desktop\PhD\Monitor\Minoo\pre-processed signals\NIRS containing transients\mydata';
+PP = 'C:\Users\PhysioUser\OneDrive - University College Cork\Monitor\Minoo\pre-processed signals\NIRS containing transients\mydata';
 SS = dir(fullfile(PP,'*.txt')); 
-for k = 1:numel(S)
+for k = 42 %61:numel(S)
+    clear name
     clear table
     F = fullfile(P,S(k).name);
     S(k).data = readtable(F);
@@ -10,11 +11,14 @@ for k = 1:numel(S)
     FF = fullfile(PP,SS(k).name);
     SS(k).data = readtable(FF);
     mydata = table2array(SS(k).data)
-    sat = mydata(:,2)
-    transient(transient >= mode(transient)) = NaN
+    sat = mydata(1:3601,2) %to separate the first 6 hours
+    base = mode(transient)
+    transient(transient >= base) = NaN
+    transient = transient(1:3601,:) %to separate the first 6 hours of the transient
     total_pow = nanmean(abs(transient).^2 )
     transient = table2array(S(k).data)
-    transient(transient> mode(transient)) = mode(transient)
+    transient(transient> base) = base
+    transient = transient(1:3601,:) %to separate the first 6 hours of the transient
     env = (abs(hilbert(transient))).^2
     meanenv = mean(env)
     SDenv = std(env)
@@ -28,7 +32,7 @@ for k = 1:numel(S)
     mobility2 = sqrt(m4 / m2);
     complexity = mobility2 / mobility;
     %to measure inter spike interval
-    transient(transient< mode(transient)) = "NaN"
+    transient(transient< base) = "NaN"
     y = length(transient)
 %     transient(7738,:) = NaN
 %     transient(16919,:) = mode(transient)
@@ -65,7 +69,7 @@ for k = 1:numel(S)
             end 
         end
         d = find(b==1)
-        time = mydata(:,1)
+        time = mydata(1:3601,1) %to separate the first 6 hours of data
         time = [time(1,1); time(d); time(end,1)]
         time = diff(time)
         isi = time(1:2:end,:)
@@ -89,36 +93,75 @@ for k = 1:numel(S)
     end
     %to measure the time spent below 63%
     transient = table2array(S(k).data)
+    transient = transient(1:3601,:) %to separate the first 6 hours of the transient
     transient(transient< 63) = "NaN"
 %     transient(17029,:) = 63.0570
     y = length(transient)
     b = zeros(y,1)
-    if isnan(transient(1,1)) == 1
+    for x = 2:y-1
+        g = find((isnan(transient(x-1,1)) == 1) && (isnan(transient(x+1,1)) == 1) && (isnan(transient(x,1)) == 0))
+        if g == 1
+            transient(x,1) = NaN
+        end 
+    end 
+    for x = 2:y-1
+        g = find((isnan(transient(x-1,1)) == 0) && (isnan(transient(x+1,1)) == 0) && (isnan(transient(x,1)) == 1))
+        if g == 1
+            transient(x,1) = transient(x-1, 1) 
+        end 
+    end
+    if find(b == 1) ~= 0 
+        disp(k)
+        error('error in 63%')
+    end
+    b = zeros(y,1)
+    if isnan(transient(1,1)) == 1 && isnan(transient(2,1)) == 0
         for x = 2:y-1
-            a = find((isnan(transient(x,1)) == 1) && (isnan(transient(x-1,1)) == 0))
-            if a == 1
-            b(x,1) = a
-            end   
-        end
-        for x = 2:y-1
-            c = find((isnan(transient(x,1)) == 1) && (isnan(transient(x+1,1)) == 0))
-            if c == 1
-            b(x,1) = c
-            end 
-        end
-        d = find(b==1)
-        time = mydata(:,1)
-        time = [time(1,1); time(d); time(end,1)]
-        time = diff(time)
-        below63 = time(3:2:end,:)
-        below63 = sum(below63)
-    else 
-        for x = 1:y-1
             a = find((isnan(transient(x,1)) == 1) && (isnan(transient(x-1,1)) == 0))
             if a == 1
                 b(x,1) = a
             end   
         end
+        for x = 2:y-1
+            c = find((isnan(transient(x,1)) == 1) && (isnan(transient(x+1,1)) == 0))
+            if c == 1
+                b(x,1) = c
+            end 
+        end
+        d = find(b==1)
+        time = mydata(1:3601,1) %to separate the first 6 hours
+        time = [time(1,1); time(d); time(end,1)]
+        time = diff(time)
+        below63 = time(2:2:end,:)
+        below63 = sum(below63)
+    end 
+    if isnan(transient(1,1)) == 1 && isnan(transient(2,1)) == 1
+        for x = 2:y-1
+            a = find((isnan(transient(x,1)) == 1) && (isnan(transient(x-1,1)) == 0))
+            if a == 1
+                b(x,1) = a
+            end   
+        end
+        for x = 2:y-1
+            c = find((isnan(transient(x,1)) == 1) && (isnan(transient(x+1,1)) == 0))
+            if c == 1
+                b(x,1) = c
+            end 
+        end
+        d = find(b==1)
+        time = mydata(1:3601,1) %to separate the first 6 hours
+        time = [time(1,1); time(d); time(end,1)]
+        time = diff(time)
+        below63 = time(3:2:end,:)
+        below63 = sum(below63)
+    end 
+    if isnan(transient(1,1)) == 0
+        for x = 1:y-1
+            a = find((isnan(transient(x,1)) == 1) && (isnan(transient(x-1,1)) == 0))
+            if a == 1
+                b(x,1) = a
+            end   
+        end 
         for x = 1:y-1
             c = find((isnan(transient(x,1)) == 1) && (isnan(transient(x+1,1)) == 0))
             if c == 1
@@ -126,7 +169,7 @@ for k = 1:numel(S)
             end 
         end
         d = find(b==1)
-        time = mydata(:,1)
+        time = mydata(1:3601,1) %to separate the first 6 hours
         time = [time(1,1); time(d); time(end,1)]
         time = diff(time)
         below63 = time(2:2:end,:)
@@ -155,7 +198,24 @@ for k = 1:numel(S)
     sat(sat<= 63) = "NaN"
     y = length(sat)
     b = zeros(y,1)
-    if isnan(sat(1,1)) == 1 
+    for x = 2:y-1
+        g = find((isnan(sat(x-1,1)) == 1) && (isnan(sat(x+1,1)) == 1) && (isnan(sat(x,1)) == 0))
+        if g == 1
+            sat(x,1) = NaN
+        end 
+    end 
+    for x = 2:y-1
+        g = find((isnan(sat(x-1,1)) == 0) && (isnan(sat(x+1,1)) == 0) && (isnan(sat(x,1)) == 1))
+        if g == 1
+            sat(x,1) = sat(x-1, 1) 
+        end 
+    end
+    if find(b == 1) ~= 0 
+        disp(k)
+        error('error in 63% in main signal')
+    end
+    b = zeros(y,1)
+    if isnan(sat(1,1)) == 1 && isnan(sat(2,1)) == 0
         for x = 2:y-1
             a = find((isnan(sat(x,1)) == 1) && (isnan(sat(x-1,1)) == 0))
             if a == 1
@@ -169,12 +229,33 @@ for k = 1:numel(S)
             end 
         end
         d = find(b==1)
-        time = mydata(:,1)
+        time = mydata(1:3601,1) %to separate the first 6 hours
+        time = [time(1,1); time(d); time(end,1)]
+        time = diff(time)
+        below63_rcSO2 = time(2:2:end,:)
+        below63_rcSO2 = sum(below63_rcSO2)
+    end 
+    if isnan(sat(1,1)) == 1 && isnan(sat(2,1)) == 1
+        for x = 2:y-1
+            a = find((isnan(sat(x,1)) == 1) && (isnan(sat(x-1,1)) == 0))
+            if a == 1
+                b(x,1) = a
+            end   
+        end
+        for x = 2:y-1
+            c = find((isnan(sat(x,1)) == 1) && (isnan(sat(x+1,1)) == 0))
+            if c == 1
+                b(x,1) = c
+            end 
+        end
+        d = find(b==1)
+        time = mydata(1:3601,1) %to separate the first 6 hours
         time = [time(1,1); time(d); time(end,1)]
         time = diff(time)
         below63_rcSO2 = time(3:2:end,:)
         below63_rcSO2 = sum(below63_rcSO2)
-    else 
+    end 
+    if isnan(sat(1,1)) == 0
         for x = 1:y-1
             a = find((isnan(sat(x,1)) == 1) && (isnan(sat(x-1,1)) == 0))
             if a == 1
@@ -188,7 +269,7 @@ for k = 1:numel(S)
             end 
         end
         d = find(b==1)
-        time = mydata(:,1)
+        time = mydata(1:3601,1) %to separate the first 6 hours
         time = [time(1,1); time(d); time(end,1)]
         time = diff(time)
         below63_rcSO2 = time(2:2:end,:)
@@ -214,8 +295,9 @@ for k = 1:numel(S)
     end
     %to calculate bandwidth ans slope
     transient = table2array(S(k).data)
-    time = mydata(:,1)
-    base = mode(transient)
+    %base = nanmean(transient)
+    transient = transient(1:3601,:) %to separate the first 6 hours
+    time = mydata(1:3601,1) %to separate the first 6 hours 
     transient(transient>= base) = "NaN"
 %     transient(7738,:) = mode(transient)
 %     transient(16919,:) = NaN
@@ -237,7 +319,7 @@ for k = 1:numel(S)
         d = find(b==1)
         starts = d(1:2:end)
         stops = d(2:2:end)
-%         starts(end,:)=[]
+        %starts(end,:)=[]
         if length(starts) ~= length(stops)
             error('starts and stops are not equal')
         end
@@ -256,7 +338,7 @@ for k = 1:numel(S)
         amp = base - y
         slope_down = (y - base)./x
         slope_up = (base-y)./(length(a)-sum(isnan(a))-x)
-        time = mydata(:,1)
+        time = mydata(1:3601,1) %to separate the first 6 hours
         time = [time(1,1); time(d); time(end,1)]
         time = diff(time)
         width = time(2:2:end,:)
@@ -274,8 +356,10 @@ for k = 1:numel(S)
             end 
         end
         d = find(b==1)
-        starts = d(2:2:end)
-        stops = d(3:2:end)
+        %starts = d(2:2:end)
+        starts = [1,150,872,2092]
+        %stops = d(3:2:end)
+        stops = [102,825,2060,3601]
         if length(starts) ~= length(stops)
             error('starts and stops are not equal')
             print(F)
@@ -296,7 +380,7 @@ for k = 1:numel(S)
         amp = base - y
         slope_down = (y - base)./x
         slope_up = (base-y)./(length(a)-sum(isnan(a))-x)
-        time = mydata(:,1)
+        time = mydata(1:3601,1) %to separate the first 6 hours
         time = [time(1,1); time(d); time(end,1)]
         time = diff(time)
         width = time(3:2:end,:)
@@ -323,6 +407,6 @@ for k = 1:numel(S)
     end
     %to combine all the features in one table and save it as text file
     table = table(total_pow, meanenv, SDenv, activity, mobility, complexity, mean(isi), std(isi), below63, mean(amp), mean(slope_down.'), mean(slope_up.'), mean(width), length(starts), below63_rcSO2)
-    name = append('transient_features', '_' ,  erase(S(k).name, 'mydata_'))
+    name = append('transient_6h_features', '_' ,  erase(S(k).name, 'mydata_'))
     writetable(table, name, 'Delimiter', 'tab')
 end 
